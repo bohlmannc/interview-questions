acb06bcc8002285d32b4124b3be3894c9297c336
Christopher Bohlmann <44445083+bohlmannc@users.noreply.github.com>
2019-09-20 11:11:49 -0400

    Update string_sort.py

--- string_sort.py
+++ string_sort.py
@@ -1,39 +1,4 @@
-'''
-
-def string_pattern(string, pattern):
-    string_count = []
-    pattern_count = []
-
-
-    string_letters = []
-    pattern_letters = []
-
-    a = len(string)
-    b = len(pattern)
-    pointer = max(a, b)
-    print(pointer)
-
-    for i in range(len(string)):
-        letter = string[i]
-        string_letters.append(letter)
-        diff = ord(letter) - ord('a')
-        string_count.append(diff)
-
-    for j in range(len(pattern)):
-        letter2 = pattern[j]
-        pattern_letters.append(letter2)
-        diff2 = ord(letter2) - ord('a')
-        pattern_count.append(diff2)
-
-    # return string_count, pattern_count
-    print(string_count)
-    print(pattern_count)
-
-    print(string_letters)
-    print(pattern_letters)
-'''
-
-def sort_string(string, pattern):
+def sort_string(string, pattern): # naive implementation
     s = string
     output = ''
 
@@ -45,7 +10,7 @@ def sort_string(string, pattern):
     return output + s
 
 
-def sort_string2(string, pattern):
+def sort_string2(string, pattern): # better implementation using a dictionary (map)
     fmap = {}
     s = string
     for char in string:
@@ -57,6 +22,7 @@ def sort_string2(string, pattern):
             res = ''.join([char * fmap[char] for char in fmap])[::-1]
     return ''.join((res, s))
 
+
 if __name__ == "__main__":
     s = 'googled'
     p = 'dog'
@@ -64,37 +30,6 @@ if __name__ == "__main__":
     p1 = 'try'
     print(sort_string2(s, p))
 
-
-'''
-    index = 0
-    while index < pointer:
-        d = ord(letter) - ord(letter2)
-        if d == 0:
-            
-
-    res = []
-    for m in range(len(pattern_count)):
-        p = pattern_count[m]
-        for n in range(len(string_count)):
-            s = string_count[n]
-            if p == s:
-                res.append(s)
-                m += 1
-            else:
-                n += 1
-
-    res.extend(string_count[m:-1])
-    out = []
-    for x in range(len(res)):
-        temp = res[x] + 97
-        out.append(temp)
-    outs = ''
-    for z in range(len(out)):
-        tmp = chr(out[z])
-        outs = outs + tmp
-
-    return outs
-'''
 
 if __name__ == "__main__":
     string = 'googled'
